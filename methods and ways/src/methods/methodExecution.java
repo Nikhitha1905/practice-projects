ec8e86ff9bf15f4a27da596204eeb2bb26c8da6f
julakanti nikhitha <narayangoud05@gmail.com>
2023-05-09 17:05:04 +0530

    commit

---
+++ methods and ways/src/methods/methodExecution.java
@@ -0,0 +1,47 @@
+package methods;
+
+
+	//method demo
+	public class methodExecution {
+
+	public int multipynumbers(int a,int b) {
+		int z=a*b;
+		return z;
+	}
+	
+	public static void main (String[] args) {
+
+		methodExecution b=new methodExecution();
+		int ans= b.multipynumbers(10,3);
+		System.out.println("Multipilcation is :"+ans);
+    }
+	
+	}
+
+
+ /*public static void main(String args[]) {
+	callMethod d = new callMethod();
+	System.out.println("Before operation value of data is "+d.val);
+	d.operation(100);
+	System.out.println("After operation value of data is "+d.val);
+	}
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	  
+
+
+
+
